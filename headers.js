886d834866aec8d0fa4f00bfb3ef4a779399ca40
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-02-27 21:57:20 +0900

    add effect on carousel

--- headers.js
+++ headers.js
@@ -6,3 +6,11 @@ const fadeInTimeline = gsap.timeline()
 fadeInTimeline
   .set(carousels, { opacity: 0 })
   .to(carousels, { opacity: 1, delay: 1, stagger: 1, durawtion: 3 })
+
+const movementTimeline = gsap.timeline({
+  repeat: -1
+})
+
+movementTimeline
+  .set(carousels, { x: 0 })
+  .to(carousels, { x: -400, duration: 6, ease: 'linear' })
