893255a9afb7734e4643d9f5b5edff20048bba39
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-02-27 21:22:45 +0900

    add second section and switch on both

--- slides.js
+++ slides.js
@@ -1,13 +1,21 @@
-const slides = document.querySelector('section div.slides')
-const images = slides.querySelectorAll('img')
+const slides = document.querySelectorAll('section div.slides')
 
-let current = 0
-let z = 0
+slides.forEach((slide) => {
+  const images = slide.querySelectorAll('img')
 
-slides.addEventListener('click', function (e) {
-  z++
-  images[current].style.zIndex = z
-  current++
-  current = current % images.length
-  console.log(e)
+  let current = 0
+  let z = 1000000
+
+  images.forEach((image) => {
+    z = z - 1
+    image.style.zIndex = z
+  })
+
+  slide.addEventListener('click', function (e) {
+    z++
+    images[current].style.zIndex = z
+    current++
+    current = current % images.length
+    console.log(e)
+  })
 })
