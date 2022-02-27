8e39ab1bb7532eb2ed77b1f0a6db6a2e83e27ce7
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-02-27 22:18:30 +0900

    finish carousel on header

--- headers.js
+++ headers.js
@@ -1,16 +1,46 @@
-console.log('works')
-const carousels = document.querySelectorAll('header h1, header h2')
+const carouselsOne = document.querySelectorAll('header h1')
+const carouselsTwo = document.querySelectorAll('header h2')
 
 const fadeInTimeline = gsap.timeline()
 
 fadeInTimeline
-  .set(carousels, { opacity: 0 })
-  .to(carousels, { opacity: 1, delay: 1, stagger: 1, durawtion: 3 })
+  .set(carouselsOne, { opacity: 0 })
+  .to(carouselsOne, { opacity: 1, delay: 1, stagger: 1, duration: 3 })
 
-const movementTimeline = gsap.timeline({
-  repeat: -1
+carouselsOne.forEach((carousel) => {
+  const spanTag = carousel.querySelector('span')
+  const spanWidth = spanTag.clientWidth
+
+  for (let i = 0; i < 20; i++) {
+    carousel.appendChild(spanTag.cloneNode(true))
+  }
+
+  const movementTimeline = gsap.timeline({
+    repeat: -1
+  })
+
+  movementTimeline
+    .set(carouselsOne, { x: 0 })
+    .to(carouselsOne, { x: spanWidth * -1, duration: 6, ease: 'linear' })
 })
 
-movementTimeline
-  .set(carousels, { x: 0 })
-  .to(carousels, { x: -400, duration: 6, ease: 'linear' })
+fadeInTimeline
+  .set(carouselsTwo, { opacity: 0 })
+  .to(carouselsTwo, { opacity: 1, delay: 1, stagger: 1, duration: 3 })
+
+carouselsTwo.forEach((carousel) => {
+  const spanTag = carousel.querySelector('span')
+  const spanWidth = spanTag.clientWidth
+
+  for (let i = 0; i < 20; i++) {
+    carousel.appendChild(spanTag.cloneNode(true))
+  }
+
+  const movementTimeline = gsap.timeline({
+    repeat: -1
+  })
+
+  movementTimeline
+    .set(carouselsTwo, { x: 0 })
+    .to(carouselsTwo, { x: spanWidth * -1, duration: 6, ease: 'linear' })
+})
